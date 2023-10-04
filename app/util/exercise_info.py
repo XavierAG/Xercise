d32f5bd1dec4be66f92f9c944e39183c57f33b39
Xavier Guzman <xavierart2001@gmail.com>
2023-10-04 02:10:57 -0500

    added aws to forms

---
+++ app/util/exercise_info.py
@@ -0,0 +1,22 @@
+exercise_categories = [
+    ('barbell', 'Barbell'),
+    ('dumbell', 'Dumbell'),
+    ('machine/other', 'Machine/Other'),
+    ('weighted bodyweight', 'Weighted Bodyweight'),
+    ('assisted bodyweight', 'Assisted Bodyweight'),
+    ('peps only', 'Reps Only'),
+    ('cardio', 'Cardio'),
+    ('duration', 'duration')
+]
+
+body_part = [
+    ('core', 'Core'),
+    ('arms', 'Arms'),
+    ('chest', 'Chest'),
+    ('back', 'Back'),
+    ('legs', 'Legs'),
+    ('shoulders', 'Shoulders'),
+    ('other', 'Other'),
+    ('full body', 'Full Body'),
+
+]
